f634d4a1f0828402c7f010d592a8ea780a653dba
Matrix030 <rishigharat@gmail.com>
2026-02-08 13:27:17 -0500

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,22 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
-import Skills from './components/Skills';
+// import Skills from './components/Skills';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 import CurrentTechnologies from './components/CurrentTechnologies';
 
 export default function Home() {
-  return (
-    <main id="main-content" className="overflow-hidden">
-      <Navbar />
-      <Hero />
-      <CurrentTechnologies />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
-      <Footer />
-    </main>
-  );
+	return (
+		<main id="main-content" className="overflow-hidden">
+			<Navbar />
+			<Hero />
+			<CurrentTechnologies />
+			<About />
+			<Projects />
+			{/* <Skills /> */}
+			<Contact />
+			{/* <Footer /> */}
+		</main>
+	);
 }
